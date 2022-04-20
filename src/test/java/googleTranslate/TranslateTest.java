0e287b9b08030753be6b7f1504c6a13be74c6602
zhanna sencenko <jannas93@mail.ru>
2022-04-20 18:39:23 +0300

    added EnumSourse and ValueSourse

--- src/test/java/googleTranslate/TranslateTest.java
+++ src/test/java/googleTranslate/TranslateTest.java
@@ -3,8 +3,13 @@ package googleTranslate;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
@@ -20,7 +25,7 @@ public class TranslateTest {
     delimiter = '|')
 
     @ParameterizedTest (name = "Проверка перевода слова {0} с русского на английский")
-    void flightSearch(String testData, String results) {
+    void translate(String testData, String results) {
         //Предусловие
         Selenide.open("https://translate.google.com/?sl=ru&tl=en&op=translate");
 
@@ -33,4 +38,60 @@ public class TranslateTest {
                 .find(Condition.text(results))
                 .shouldBe(visible);
     }
+
+
+    @ParameterizedTest(name = "Проверка наличия {0} языка оригинала")
+    @ValueSource(strings = {
+            "испанский",
+            "тайский"
+    })
+    void languageSearch(String language) {
+        //Предусловие
+        Selenide.open("https://translate.google.com");
+
+        //Шаги
+        $(".VfPpkd-Bz112c-RLmnJb").click();
+
+        //ОР
+        $$(".vSUSRc")
+                .find(Condition.text(language))
+                .shouldBe(visible);
+    }
+
+
+   public enum TypesOfTranslation {
+        Doc("Документы"), Text("Текст"), Web("Сайты");
+
+       public final String rusName;
+
+       TypesOfTranslation(String rusName) {
+           this.rusName = rusName;
+       }
+   }
+
+    @EnumSource(TypesOfTranslation.class)
+
+    @ParameterizedTest()
+    void typesOfTranslationTest(TypesOfTranslation testData) {
+        //Предусловие
+        Selenide.open("https://translate.google.com");
+
+        //Шаги
+        $$(".U0xwnf")
+                .find(Condition.text(String.valueOf(testData.rusName)))
+                .click();
+
+        //ОР
+        Assertions.assertEquals(
+                1,
+                WebDriverRunner.getWebDriver().getWindowHandles().size()
+        );
+    }
+
+    @AfterEach
+    void close() {
+        Selenide.closeWebDriver();
+    }
+
 }
+
